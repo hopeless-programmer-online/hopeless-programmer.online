ce0e6b71d8ffbb03290b8c574f496334c352969e
Hopeless Programmer <me@hopeless-programmer.online>
2020-04-10 17:14:02 +0300

    Adding .eslint.js

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+// see https://eslint.org/docs/rules/
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "backtick"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
