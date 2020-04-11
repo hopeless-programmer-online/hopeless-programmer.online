7463c71b4bbc43e17d1be7726f7c5e4cf0c9f399
Hopeless Programmer <me@hopeless-programmer.online>
2020-04-11 08:35:39 +0300

    Adding title

--- src/documenting/section.js
+++ src/documenting/section.js
@@ -1,8 +1,12 @@
 class Section {
-    constructor({ Parts = [] } = {}) {
+    constructor({ Title, Parts = [] }) {
+        this.__title = Title;
         this.__parts = Parts;
     }
 
+    get Title() {
+        return this.__title;
+    }
     get Parts() {
         return this.__parts;
     }
