73ad219df7de4ec6a6378ae36dfdf1e7464d7c85
Hopeless Programmer <me@hopeless-programmer.online>
2020-04-11 07:14:07 +0300

    Adding phrases getter

--- src/documenting/sentence.js
+++ src/documenting/sentence.js
@@ -1,5 +1,30 @@
 class Sentence {
+    constructor({ Phrases = [] } = {}) {
+        if (Phrases instanceof Array); else {
+            throw new Error(); // @todo
+        }
+        if (Phrases.every(phrase => phrase instanceof Phrase)); else {
+            throw new Error(); // @todo
+        }
+
+        /**
+         * @private
+         * @type    {Array<Phrase>}
+         */
+        this.__phrases = Phrases;
+    }
+
+    /**
+     * @public
+     * @type   {Array<Phrase>}
+     */
+    get Phrases() {
+        return this.__phrases;
+    }
 }
 
 
 exports = module.exports = Sentence;
+
+
+const Phrase = require(`./phrase`);
