2558b1879ffe9f903e929e624164428f0a047446
Hopeless Programmer <me@hopeless-programmer.online>
2020-04-11 09:30:54 +0300

    Adding more shortcuts

--- src/documenting/shortcuts.js
+++ src/documenting/shortcuts.js
@@ -1,7 +1,20 @@
+const Document = require(`./document`);
+const Section = require(`./section`);
+const SectionPart = require(`./section-part`);
+const ParagraphSectionPart = require(`./paragraph-section-part`);
+const IllustrationsSectionPart = require(`./illustrations-section-part`);
+const Illustration = require(`./illustration`);
+const Paragraph = require(`./paragraph`);
+const Sentence = require(`./sentence`);
+const Phrase = require(`./phrase`);
 const TextPhrase = require(`./text-phrase`);
+const Code = require(`./code`);
+const CodeLine = require(`./code-line`);
+const Lexeme = require(`./lexeme`);
+const TextLexeme = require(`./text-lexeme`);
 
 
-function toTextPhrase(something) {
+function textPhrase(something) {
     if (typeof something === `string`) {
         return new TextPhrase({
             String : something,
@@ -10,6 +23,46 @@ function toTextPhrase(something) {
 
     throw new Error(); // @todo
 }
+function toTextPhrase(something) {
+    if (something instanceof TextPhrase) {
+        return something;
+    }
+
+    return textPhrase(something);
+}
+function toTextPhrases(somethings) {
+    return somethings.map(toTextPhrase);
+}
+function sentence(...somethings) {
+    const phrases = toTextPhrases(somethings);
+
+    return new Sentence({
+        Phrases : phrases,
+    });
+}
+function toSentence(something) {
+    if (something instanceof Sentence) {
+        return something;
+    }
+
+    return sentence(something);
+}
+function toSentences(somethings) {
+    return somethings.map(toSentence);
+}
+function paragraph(...somethings) {
+    const sentences = toSentences(somethings);
+
+    return new Paragraph({
+        Sentences : sentences,
+    });
+}
 
 
+exports.textPhrase = textPhrase;
 exports.toTextPhrase = toTextPhrase;
+exports.toTextPhrases = toTextPhrases;
+exports.sentence = sentence;
+exports.toSentence = toSentence;
+exports.toSentences = toSentences;
+exports.paragraph = paragraph;
---
+++ src/documenting/shortcuts.test.js
@@ -0,0 +1,24 @@
+const Paragraph = require(`./paragraph`);
+const TextPhrase = require(`./text-phrase`);
+const shortcuts = require(`./shortcuts`);
+
+
+it(`Should create Paragraph on calling paragraph`, () => {
+    const paragraph = shortcuts.paragraph(
+        `first sentence`,
+        `second sentence`,
+        `third sentence`,
+    );
+
+    expect(paragraph).toBeInstanceOf(Paragraph);
+    expect(paragraph.Sentences).toHaveLength(3);
+    expect(paragraph.Sentences[0].Phrases).toHaveLength(1);
+    expect(paragraph.Sentences[0].Phrases[0]).toBeInstanceOf(TextPhrase);
+    expect(paragraph.Sentences[0].Phrases[0].String).toBe(`first sentence`);
+    expect(paragraph.Sentences[1].Phrases).toHaveLength(1);
+    expect(paragraph.Sentences[1].Phrases[0]).toBeInstanceOf(TextPhrase);
+    expect(paragraph.Sentences[1].Phrases[0].String).toBe(`second sentence`);
+    expect(paragraph.Sentences[2].Phrases).toHaveLength(1);
+    expect(paragraph.Sentences[2].Phrases[0]).toBeInstanceOf(TextPhrase);
+    expect(paragraph.Sentences[2].Phrases[0].String).toBe(`third sentence`);
+});
