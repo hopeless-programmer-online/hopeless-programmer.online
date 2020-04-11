d4fc95915fee14a226dcbff4e61427552b83223c
Hopeless Programmer <me@hopeless-programmer.online>
2020-04-11 07:55:42 +0300

    Adding title

--- src/documenting/document.js
+++ src/documenting/document.js
@@ -1,4 +1,11 @@
 class Document {
+    constructor({ Title }) {
+        this.__title = Title;
+    }
+
+    get Title() {
+        return this.__title;
+    }
 }
 
 exports = module.exports = Document;
