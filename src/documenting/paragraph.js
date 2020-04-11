a4f8c3c3f1fed96873009966981e5d702913c769
Hopeless Programmer <me@hopeless-programmer.online>
2020-04-11 07:50:38 +0300

    Adding sentences getter

--- src/documenting/paragraph.js
+++ src/documenting/paragraph.js
@@ -1,5 +1,30 @@
 class Paragraph {
+    constructor({ Sentences = [] } = {}) {
+        if (Sentences instanceof Array); else {
+            throw new Error(); // @todo
+        }
+        if (Sentences.every(sentence => sentence instanceof Sentence)); else {
+            throw new Error(); // @todo
+        }
+
+        /**
+         * @private
+         * @type    {Array<Sentence>}
+         */
+        this.__sentences = Sentences;
+    }
+
+    /**
+     * @public
+     * @type   {Array<Sentence>}
+     */
+    get Sentences() {
+        return this.__sentences;
+    }
 }
 
 
 exports = module.exports = Paragraph;
+
+
+const Sentence = require(`./sentence`);
--- src/documenting/paragraph.test.js
+++ src/documenting/paragraph.test.js
@@ -1,6 +1,49 @@
 const Paragraph = require(`./paragraph`);
+const Sentence = require(`./sentence`);
 
 
 it(`Should be function`, () => {
     expect(Paragraph).toBeInstanceOf(Function);
+});
+it(`Should returns object on creating without arguments`, () => {
+    expect(new Paragraph).toBeInstanceOf(Paragraph);
+});
+it(`Should returns object on creating with empty arguments`, () => {
+    expect(new Paragraph({})).toBeInstanceOf(Paragraph);
+});
+it(`Should returns object on creating with empty array as "Sentences" argument`, () => {
+    expect(new Paragraph({ Sentences : [] })).toBeInstanceOf(Paragraph);
+});
+it(`Should throws on creating with non array "Sentences" argument`, () => {
+    expect(() => new Paragraph({ Sentences : 1 })).toThrowError(Error);
+});
+it(`Should throws on creating with incorrect "Sentences" argument`, () => {
+    expect(() => new Paragraph({ Sentences : [ 1 ] })).toThrowError(Error);
+});
+it(`Should returns object on creating`, () => {
+    const sentences = [
+        new Sentence,
+    ];
+
+    expect(new Paragraph({ Sentences : sentences })).toBeInstanceOf(Paragraph);
+});
+it(`Should returns empty array on getting "Sentences" after creating without arguments`, () => {
+    const paragraph = new Paragraph;
+
+    expect(paragraph.Sentences).toHaveLength(0);
+});
+it(`Should returns empty array on getting "Sentences" after creating with empty arguments`, () => {
+    const paragraph = new Paragraph({});
+
+    expect(paragraph.Sentences).toHaveLength(0);
+});
+it(`Should returns array of Sentences after creating`, () => {
+    const sentences = [
+        new Sentence,
+    ];
+    const paragraph = new Paragraph({
+        Sentences : sentences,
+    });
+
+    expect(paragraph.Sentences).toBe(sentences);
 });
