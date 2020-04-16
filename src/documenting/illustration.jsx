bc98d5a4a859a8c1e664ba82b6bce2376abe39c5
Hopeless Programmer <me@hopeless-programmer.online>
2020-04-16 15:02:06 +0300

    Adding type checks

--- src/documenting/illustration.jsx
+++ src/documenting/illustration.jsx
@@ -1,24 +1,81 @@
 class Illustration {
+    /**
+     * @param  {Object}    object
+     * @param  {Paragraph} object.Title
+     * @param  {Paragraph} object.Description
+     * @param  {number}    object.Index
+     * @param  {Content}   object.Content
+     * @throws {Error}
+     */
     constructor({ Title, Description, Index, Content }) {
+        if (Title instanceof Paragraph); else {
+            throw new Error; // @todo
+        }
+        if (Description instanceof Paragraph); else {
+            throw new Error; // @todo
+        }
+        if (typeof Index !== `number`); else {
+            throw new Error; // @todo
+        }
+        if (Content instanceof ContentClass); else {
+            throw new Error; // @todo
+        }
+
+        /**
+         * @private
+         * @type    {Paragraph}
+         */
         this.__title = Title;
+        /**
+         * @private
+         * @type    {Paragraph}
+         */
         this.__description = Description;
+        /**
+         * @private
+         * @type    {number}
+         */
         this.__index = Index;
+        /**
+         * @private
+         * @type    {Content}
+         */
         this.__content = Content;
     }
 
+    /**
+     * @public
+     * @type   {Paragraph}
+     */
     get Title() {
         return this.__title;
     }
+    /**
+     * @public
+     * @type   {Paragraph}
+     */
     get Description() {
         return this.__description;
     }
+    /**
+     * @public
+     * @type   {number}
+     */
     get Index() {
         return this.__index;
     }
+    /**
+     * @public
+     * @type   {Content}
+     */
     get Content() {
         return this.__content;
     }
 
+    /**
+     * @public
+     * @returns {html.Element}
+     */
     toHtml() {
         const id = `illustration-${this.Index}`;
 
@@ -44,3 +101,8 @@ exports = module.exports = Illustration;
 
 
 const html = require(`../html`);
+const Paragraph = require(`./paragraph`);
+const Content = require(`./illustration-content`);
+
+
+const ContentClass = Content;
--- src/documenting/illustrations-section-part.jsx
+++ src/documenting/illustrations-section-part.jsx
@@ -2,16 +2,40 @@ const SectionPart = require(`./section-part`);
 
 
 class IllustrationsSectionPart extends SectionPart {
-    constructor({ Illustrations }) {
+    /**
+     * @param  {Object}              object
+     * @param  {Array<Illustration>} object.Illustrations
+     * @throws {Error}
+     */
+    constructor({ Illustrations = [] } = {}) {
+        if (Illustrations instanceof Array); else {
+            throw new Error; // @todo
+        }
+        if (Illustrations.every(illustration => illustration instanceof Illustration)); else {
+            throw new Error; // @todo
+        }
+
         super();
 
+        /**
+         * @private
+         * @type    {Array<Illustration>}
+         */
         this.__illustrations = Illustrations;
     }
 
+    /**
+     * @public
+     * @type   {Array<Sentence>}
+     */
     get Illustrations() {
         return this.__illustrations;
     }
 
+    /**
+     * @public
+     * @returns {html.Element}
+     */
     toHtml() {
         return (
             <div class="illustrations">
@@ -26,3 +50,4 @@ exports = module.exports = IllustrationsSectionPart;
 
 
 const html = require(`../html`);
+const Illustration = require(`./illustration`);
