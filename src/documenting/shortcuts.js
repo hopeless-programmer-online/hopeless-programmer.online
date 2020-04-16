60bd618b68bff0f0e2db7208944a7f0eaa90cfa7
Hopeless Programmer <me@hopeless-programmer.online>
2020-04-16 06:44:18 +0300

    Adding shortcuts

--- src/documenting/shortcuts.js
+++ src/documenting/shortcuts.js
@@ -1,1 +1,32 @@
+const PhraseTemplate = require(`./phrase-template`);
 const TextPhraseTemplate = require(`./text-phrase-template`);
+
+
+/**
+ * @param   {string}         something
+ * @returns {PhraseTemplate}
+ */
+function phrase(something) {
+    if (typeof something === `string`) {
+        return new TextPhraseTemplate({
+            String : something,
+        });
+    }
+
+    throw new Error; // @todo
+}
+/**
+ * @param   {string | PhraseTemplate} something
+ * @returns {PhraseTemplate}
+ */
+function toPhrase(something) {
+    if (something instanceof PhraseTemplate) {
+        return something;
+    }
+
+    return phrase(something);
+}
+
+
+exports.phrase = phrase;
+exports.toPhrase = toPhrase;
