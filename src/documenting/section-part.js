a1903e5f94c0d368d9427d8c6ef58e085f38c507
Hopeless Programmer <me@hopeless-programmer.online>
2020-04-16 07:37:21 +0300

    Fixing section part

--- src/documenting/section-part.js
+++ src/documenting/section-part.js
@@ -1,5 +1,50 @@
+/**
+ * @abstract
+ */
 class SectionPart {
+    /**
+     * @throws {Error}
+     */
+    constructor() {
+        if (new.target === SectionPart) {
+            throw new Error(); // @todo
+        }
+        if (this.toHtml !== SectionPart.prototype.toHtml) {
+            throw new Error; // @todo
+        }
+        if (this._toHtml === SectionPart.prototype._toHtml) {
+            throw new Error; // @todo
+        }
+    }
+
+    /**
+     * @protected
+     * @abstract
+     * @returns   {html.Element}
+     */
+    _toHtml() {
+        throw new Error; // @todo
+    }
+
+    /**
+     * @public
+     * @returns {html.Element}
+     * @throws  {Error}
+     */
+    toHtml() {
+        const element = this._toHtml();
+
+        if (element instanceof html.Element) {
+            throw new Error; // @todo
+        }
+
+        return element;
+    }
 }
 
 
 exports = module.exports = SectionPart;
+
+
+const html = require(`../html`);
+
