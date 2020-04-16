6199fe60fe1e4fa8ef090a1d384681e7f6e8aecb
Hopeless Programmer <me@hopeless-programmer.online>
2020-04-16 07:42:55 +0300

    Adding type checks

---
+++ src/documenting/paragraph-section-part.js
@@ -0,0 +1,49 @@
+const SectionPart = require(`./section-part`);
+
+
+class ParagraphSectionPart extends SectionPart {
+    /**
+     * @param {Object}    object
+     * @param {Paragraph} object.Paragraph
+     */
+    constructor({ Paragraph }) {
+        if (Paragraph instanceof ParagraphClass); else {
+            throw new Error; // @todo
+        }
+
+        super();
+
+        /**
+         * @private
+         * @type    {Paragraph}
+         */
+        this.__paragraph = Paragraph;
+    }
+
+    /**
+     * @public
+     * @type   {Paragraph}
+     */
+    get Paragraph() {
+        return this.__paragraph;
+    }
+
+    /**
+     * @protected
+     * @override
+     * @returns   {html.Element}
+     */
+    _toHtml() {
+        return this.Paragraph.toHtml();
+    }
+}
+
+
+exports = module.exports = ParagraphSectionPart;
+
+
+const html = require(`../html`);
+const Paragraph = require(`./paragraph`);
+
+
+const ParagraphClass = Paragraph;
