20766083332db1e61750846a809b4eccb183ccf7
Hopeless Programmer <me@hopeless-programmer.online>
2020-04-20 13:02:27 +0300

    Moving home

--- src/host.js
+++ src/host.js
@@ -7,19 +7,7 @@ const h = hosting;
 const s = documenting.shortcuts;
 
 
-const home = new h.DocumentResource({
-    Document : s.document(`Ласкаво просимо!`,
-        s.section(`Сайт в процесі розробки`,
-            s.paragraph(
-                `Вітаю на моєму сайті! `,
-                `Як ви вже могли помітити, він не готовий і все ще розробляється. `,
-                `Багато чого може не працювати, або працювати не так, як було задумано. `,
-                `А тому я буду радий за будь-яку інформацію щодо проблем. `,
-                `Дякую! `,
-            ),
-        ),
-    ),
-});
+const home = require(`./host/home`);
 
 
 exports = module.exports = new h.Host({
---
+++ src/host/home.js
@@ -0,0 +1,21 @@
+const hosting = require(`../hosting`);
+const documenting = require(`../documenting`);
+
+
+const h = hosting;
+const s = documenting.shortcuts;
+
+
+exports = module.exports = new h.DocumentResource({
+    Document : s.document(`Ласкаво просимо!`,
+        s.section(`Сайт в процесі розробки`,
+            s.paragraph(
+                `Вітаю на моєму сайті! `,
+                `Як ви вже могли помітити, він не готовий і все ще розробляється. `,
+                `Багато чого може не працювати, або працювати не так, як було задумано. `,
+                `А тому я буду радий за будь-яку інформацію щодо проблем. `,
+                `Дякую! `,
+            ),
+        ),
+    ),
+});
