2186f85018bafc82e76e6e39eac7117ded4b50cf
Hopeless Programmer <me@hopeless-programmer.online>
2020-04-20 14:23:30 +0300

    Moving error pages

---
+++ src/host/404.js
@@ -0,0 +1,18 @@
+const hosting = require(`../hosting`);
+const documenting = require(`../documenting`);
+
+
+const h = hosting;
+const s = documenting.shortcuts;
+
+
+exports = module.exports = new h.DocumentResource({
+    Document : s.document([ `Сторінку не знайдено.`, `Код помилки : 404.` ],
+        s.section(`У чому річ?`,
+            s.paragraph(
+                `Сторінка на яку ви намагаєтесь перейти не існує. `,
+                `Можливо вона була видалена, переміщена, або ніколи не існувала. `,
+            ),
+        ),
+    ),
+});
