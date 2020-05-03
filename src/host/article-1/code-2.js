d9d71cdfa4f0cd8053547744bb5a49302662376e
Hopeless Programmer <me@hopeless-programmer.online>
2020-05-03 20:52:18 +0300

    Adding code

---
+++ src/host/article-1/code-2.js
@@ -0,0 +1,14 @@
+const s = require(`../../documenting`).shortcuts;
+const { lt, v, cm, c } = s;
+
+
+exports = module.exports = s.illustration( ...[
+    `В C# спроба перекрити змінну приведе до помилки.`,
+    s.code(
+        [ c(`int`), ` `, v(`x`), ` = `, lt(`5`), `;` ],
+        [ `...` ],
+        [ `{` ],
+        [ `    `, c(`int`), ` `, v(`x`), ` = `, lt(`10`), `; `, cm(`// помилка`) ],
+        [ `}` ],
+    ),
+]);
---
+++ src/host/article-1/code-3.js
@@ -0,0 +1,18 @@
+const s = require(`../../documenting`).shortcuts;
+const { kw, v, f, a, cm, c, p } = s;
+
+
+exports = module.exports = s.illustration( ...[
+    `Зворотне перекриття поля класу аргументом в C#.`,
+    s.code(
+        [ kw(`class`), ` `, c(`X`), ` {` ],
+        [ `    `, c(`int`), ` `, v(`x`), `;` ],
+        [ `    ` ],
+        [ `    `, kw(`public`), ` `, c(`void`), ` `, f(`F`), `(`, c(`int`), ` `, a(`x`), `)` ],
+        [ `    {` ],
+        [ `        `, f(`print`), `(`, v(`x`), `);      `, cm(`// аргумент`) ],
+        [ `        `, f(`print`), `(`, kw(`this`), `.`, p(`x`), `); `, cm(`// поле класу`) ],
+        [ `    }` ],
+        [ `}` ],
+    ),
+]);
