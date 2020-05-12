0fc87538bb1e0763b490d5fc2ff62fedb7f0c624
Hopeless Programmer <me@hopeless-programmer.online>
2020-05-12 04:25:01 +0300

    Adding code

--- src/host/article-4/code-13.js
+++ src/host/article-4/code-13.js
@@ -2,8 +2,34 @@ const s = require(`../../documenting`).shortcuts;
 
 
 exports = module.exports = s.illustration( ...[
-    s.sentence(``),
+    s.paragraph(...[
+        s.sentence(`Метод VisitNumber повертає значення замість того, щоб зберігати його. `),
+        s.sentence(`П'ятірка виводиться лише з міркувань абстрагування від наповнення класу Number : )`),
+    ]),
     s.code(
-
+        [ s.kw(`class`), ` `, s.c(`Visitor`), ` {` ],
+        [ `    `, s.f(`Visit`), `(`, s.v(`object`), `) {` ],
+        [ `        `, s.kw(`return`), ` `, s.v(`object`), `.`, s.f(`Accept`), `(`, s.kw(`this`), `);` ],
+        [ `    }` ],
+        [ `    ...` ],
+        [ `}` ],
+        [ `...` ],
+        [ s.kw(`class`), ` `, s.c(`Number`), ` `, s.kw(`extends`), ` `, s.c(`Object`), ` {` ],
+        [ `    `, s.f(`Accept`), `(`, s.v(`visitor`), `) {` ],
+        [ `        `, s.kw(`return`), ` `, s.v(`visitor`), `.`, s.f(`VisitNumber`), `(`, s.kw(`this`), `);` ],
+        [ `    }` ],
+        [ `}` ],
+        [ `...` ],
+        [ s.kw(`class`), ` `, s.c(`JSON`), ` `, s.kw(`extends`), ` `, s.c(`Visitor`), ` {` ],
+        [ `    `, s.f(`VisitNumber`), `(`, s.v(`number`), `) { `, s.cm(`// заміщений`) ],
+        [ `        `, s.kw(`return`), ` `, s.lt(`\`5\``), `;` ],
+        [ `    }` ],
+        [ `    ...` ],
+        [ `}` ],
+        [ `...` ],
+        [ s.kw(`let`), ` `, s.v(`json`), `   = `, s.kw(`new`), ` `, s.c(`JSON`), `();` ],
+        [ s.kw(`let`), ` `, s.v(`object`), ` = `, s.kw(`new`), ` `, s.c(`Number`), `();` ],
+        [ `` ],
+        [ s.kw(`let`), ` `, s.v(`result`), ` = `, s.v(`json`), `.`, s.f(`Visit`), `(`, s.v(`object`), `); `, s.cm(`// \`5\``) ],
     ),
 ]);
--- src/host/article-4/code-17.js
+++ src/host/article-4/code-17.js
@@ -1,9 +1,33 @@
 const s = require(`../../documenting`).shortcuts;
+const { kw, cm, f, c, v, lt } = s;
 
 
 exports = module.exports = s.illustration( ...[
-    s.sentence(``),
+    s.sentence(`Тепер обробкою усіх нецікавих класів займається метод VisitOther.`),
     s.code(
-
+        [ kw(`class`), ` `, c(`Visitor`), ` {` ],
+        [ `    `, f(`VisitOther`), `(`, v(`object`), `); `, cm(`  // абстрактний`) ],
+        [ `    `, f(`VisitObject`), `(`, v(`object`), `) {`, cm(` // віртуальний`) ],
+        [ `        `, kw(`return`), ` `, kw(`this`), `.`, f(`VisitOther`), `(`, v(`object`), `);` ],
+        [ `    }` ],
+        [ `    `, f(`VisitNumber`), `(`, v(`number`), `) {`, cm(` // віртуальний`) ],
+        [ `        `, kw(`return`), ` `, kw(`this`), `.`, f(`VisitOther`), `(`, v(`number`), `);` ],
+        [ `    }` ],
+        [ `    `, f(`VisitString`), `(`, v(`string`), `) {`, cm(` // віртуальний`) ],
+        [ `        `, kw(`return`), ` `, kw(`this`), `.`, f(`VisitOther`), `(`, v(`string`), `);` ],
+        [ `    }` ],
+        [ `    ...` ],
+        [ `}` ],
+        [ `...` ],
+        [ kw(`class`), ` `, c(`JSON`), ` `, kw(`extends`), ` `, c(`Visitor`), ` {` ],
+        [ `    `, cm(`// числа`) ],
+        [ `    `, f(`VisitNumber`), `(`, v(`number`), `) {` ],
+        [ `        `, kw(`return`), ` `, lt(`"number"`), `;` ],
+        [ `    }` ],
+        [ `    `, cm(`// решта типів`) ],
+        [ `    `, f(`VisitOther`), `() {` ],
+        [ `        `, kw(`return`), ` `, lt(`""`), `;` ],
+        [ `    }` ],
+        [ `}` ],
     ),
 ]);
