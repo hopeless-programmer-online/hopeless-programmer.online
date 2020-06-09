ef4107cba6592cc471e984ddc0a6f26cd95bf201
Hopeless Programmer <me@hopeless-programmer.online>
2020-06-10 00:15:16 +0300

    Replacing images with css

--- src/server/documenting/deduce-code-header.jsx
+++ src/server/documenting/deduce-code-header.jsx
@@ -1,25 +1,26 @@
 function deduce(title, ext, language) {
-    const [ icon, extension ] =
-        language === Language.JavaScript ? [ `javascript`, `.js`   ] :
-        language === Language.JSON       ? [ `json`,       `.json` ] :
-        language === Language.C          ? [ `c`,          `.c`    ] :
-        language === Language.CPlusPlus  ? [ `cpp`,        `.cpp`  ] :
-        language === Language.CSharp     ? [ `c-sharp`,    `.cs`   ] :
-        language === Language.PHP        ? [ `php`,        `.php`  ] :
-        language === Language.HTML       ? [ `html`,       `.html` ] :
-        language === Language.CSS        ? [ `css`,        `.css`  ] :
-        language === Language.SASS       ? [ `sass`,       `.sass` ] :
-        language === Language.SCSS       ? [ `sass`,       `.scss` ] :
-        language === Language.XML        ? [ `xml`,        `.xml`  ] :
-        language === Language.Python     ? [ `python`,     `.py`   ] :
-        language === Language.TeX        ? [ `tex`,        `.tex`  ] :
-        language === Language.LLVM       ? [ `default`,    `.llvm` ] :
-        language === Language.QBasic     ? [ `default`,    `.qb`   ] :
+    const [ icon, caption, extension ] =
+        language === Language.JavaScript ? [ `javascript`, `js`,   `.js`   ] :
+        language === Language.JSON       ? [ `json`,       `json`, `.json` ] :
+        language === Language.C          ? [ `c`,          `c`,    `.c`    ] :
+        language === Language.CPlusPlus  ? [ `cpp`,        `c++`,  `.cpp`  ] :
+        language === Language.CSharp     ? [ `c-sharp`,    `c#`,   `.cs`   ] :
+        language === Language.PHP        ? [ `php`,        `php`,  `.php`  ] :
+        language === Language.HTML       ? [ `html`,       `html`, `.html` ] :
+        language === Language.CSS        ? [ `css`,        `css`,  `.css`  ] :
+        language === Language.SASS       ? [ `sass`,       `sass`, `.sass` ] :
+        language === Language.SCSS       ? [ `sass`,       `scss`, `.scss` ] :
+        language === Language.XML        ? [ `xml`,        `xml`,  `.xml`  ] :
+        language === Language.Python     ? [ `python`,     `py`,   `.py`   ] :
+        language === Language.TeX        ? [ `tex`,        `tex`,  `.tex`  ] :
+        language === Language.LLVM       ? [ `default`,    `llvm`, `.llvm` ] :
+        language === Language.QBasic     ? [ `default`,    `qb`,   `.qb`   ] :
         [ `default`, `.txt` ];
 
     return (
         <figcaption class="hp-class-code-header">
-            <img alt={icon} src={`./media/icons/${icon}.svg`} />
+            {/* <img alt={icon} src={`./media/icons/${icon}.svg`} /> */}
+            <span class="hp-class-icon" data-hp-type={icon}>{caption}|</span>
             {title + (ext === null ? extension : ``)}
         </figcaption>
     );
