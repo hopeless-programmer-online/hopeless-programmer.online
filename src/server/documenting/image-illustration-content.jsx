9b45b41847f4f49456fd70946b9f33c085aaa601
Hopeless Programmer <me@hopeless-programmer.online>
2020-06-29 10:36:18 +0300

    Adding svg handler

--- src/server/documenting/image-illustration-content.jsx
+++ src/server/documenting/image-illustration-content.jsx
@@ -34,10 +34,33 @@ class ImageIllustrationContent extends IllustrationContent {
      * @returns   {html.Element}
      */
     async _toHtml() {
+        const url = this.Url;
+
+        if (path.extname(url) === `.svg`) {
+            const full = path.join(__dirname, `../../../`, url); // @todo: fix this hack
+            const file = fs.readFileSync(full, `utf-8`);
+            const node = await svgson.parse(file);
+
+            if (hasExternals) {
+                const { width, height } = node.attributes;
+                const style = [
+                    width  ? `width: ${width}`  : null,
+                    height ? `height: ${height}` : null,
+                ].filter(x => !!x).join(`; `);
+
+                if (style) {
+                    return (
+                        <figure>
+                            <object data={this.Url} type="image/svg+xml" style={style}></object>
+                        </figure>
+                    );
+                }
+            }
+        }
+
         return (
             <figure>
-                <object data={this.Url} type="image/svg+xml" style="width: 600px;"></object>
-                {/* <img alt="image" src={this.Url}/> */}
+                <img alt="image" src={this.Url}/>
             </figure>
         );
     }
@@ -47,4 +70,20 @@ class ImageIllustrationContent extends IllustrationContent {
 exports = module.exports = ImageIllustrationContent;
 
 
+const path = require(`path`);
+const fs = require(`fs`);
+const svgson = require(`svgson`);
 const html = require(`../html`);
+
+
+/**
+ * @param   {svgson.INode} node
+ * @returns {boolean}
+ */
+async function hasExternals(node) {
+    if (node.name === `script`) {
+        return true;
+    }
+
+    return node.children.some(hasExternals);
+}
