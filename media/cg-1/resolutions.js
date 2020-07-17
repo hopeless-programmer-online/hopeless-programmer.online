95be21db3f6359232b1ad490419983ac72863083
hopeless-programmer-online <63446812+hopeless-programmer-online@users.noreply.github.com>
2020-07-17 03:14:38 +0300

    Add resolution illustration

---
+++ media/cg-1/resolutions.js
@@ -0,0 +1,235 @@
+const svg = document.currentScript.parentNode;
+
+window.addEventListener(`load`, main);
+
+function aspect({ width, height }) {
+    let d = 2;
+
+    while (d < Math.min(width, height)) {
+        if ((width % d !== 0) || (height % d !== 0)) {
+            ++d;
+
+            continue;
+        }
+
+        width /= d;
+        height /= d;
+    }
+
+    return `${width}:${height}`;
+}
+function getRect({ x, y, width, height }) {
+    return {
+        x,
+        y,
+        width,
+        height,
+        left       : x,
+        top        : y,
+        right      : x + width,
+        bottom     : y + height,
+        vertical   : x + width / 2,
+        horizontal : y + height / 2,
+    };
+}
+
+function main() {
+    const resolutions = [
+        {
+            name   : `8K UHD`,
+            width  : 7680,
+            height : 4320,
+        },
+        {
+            name   : `4K UHD`,
+            width  : 3840,
+            height : 2160,
+        },
+        {
+            name   : `QHD`,
+            width  : 2560,
+            height : 1440,
+        },
+        {
+            name   : `Full HD`,
+            width  : 1920,
+            height : 1080,
+        },
+        {
+            name   : `720p`,
+            width  : 1280,
+            height : 720,
+        },
+        {
+            name   : `480p`,
+            width  : 720,
+            height : 480,
+        },
+    ]
+        .map(resolution => ({
+            ...resolution,
+            pixels : resolution.width * resolution.height,
+            aspect : aspect(resolution),
+        }))
+        .sort((a, b) => a.mp - b.mp)
+        ;
+
+    const padding = 20;
+    const viewBox = {
+        x      : -padding,
+        y      : -padding,
+        width  : Math.max(...resolutions.map(({ width }) => width)) + padding * 2,
+        height : Math.max(...resolutions.map(({ height }) => height)) + padding * 2,
+    };
+
+    for (const { name, width, height, pixels, aspect } of resolutions) {
+        const border = document.createElementNS(`http://www.w3.org/2000/svg`, `rect`);
+
+        border.classList.add(`hp-class-border`);
+        border.setAttribute(`x`,      -padding);
+        border.setAttribute(`y`,      -padding);
+        border.setAttribute(`width`,  width + padding * 2);
+        border.setAttribute(`height`, height + padding * 2);
+
+        svg.appendChild(border);
+
+        const rect = document.createElementNS(`http://www.w3.org/2000/svg`, `rect`);
+
+        rect.classList.add(`hp-class-screen`);
+        rect.setAttribute(`x`,      0);
+        rect.setAttribute(`y`,      0);
+        rect.setAttribute(`width`,  width);
+        rect.setAttribute(`height`, height);
+
+        svg.appendChild(rect);
+
+        const size = rect.getBBox().height * 0.1;
+
+        const caption = document.createElementNS(`http://www.w3.org/2000/svg`, `text`);
+
+        caption.appendChild(document.createTextNode(name));
+        caption.classList.add(`hp-class-caption`);
+        caption.setAttribute(`x`, width - padding);
+        caption.setAttribute(`y`, height);
+        caption.style.fontSize = `${size}px`;
+
+        svg.appendChild(caption);
+
+        const c = getRect(caption.getBBox());
+        const aspectText = document.createElementNS(`http://www.w3.org/2000/svg`, `text`);
+
+        aspectText.appendChild(document.createTextNode(aspect));
+        aspectText.classList.add(`hp-class-aspect`);
+        aspectText.setAttribute(`x`, padding);
+        aspectText.setAttribute(`y`, height);
+        aspectText.style.fontSize = `${size}px`;
+
+        svg.appendChild(aspectText);
+
+        const megapixels = document.createElementNS(`http://www.w3.org/2000/svg`, `text`);
+
+        megapixels.appendChild(document.createTextNode((pixels / 1e6).toFixed(2)));
+        megapixels.classList.add(`hp-class-megapixels`);
+        megapixels.setAttribute(`x`, width - padding);
+        megapixels.setAttribute(`y`, 0);
+        megapixels.style.fontSize = `${size}px`;
+
+        svg.appendChild(megapixels);
+
+        const m = getRect(megapixels.getBBox());
+        const megapixelsText = document.createElementNS(`http://www.w3.org/2000/svg`, `text`);
+
+        megapixelsText.appendChild(document.createTextNode(`megapixels`));
+        megapixelsText.classList.add(`hp-class-megapixels`);
+        megapixelsText.setAttribute(`x`, width - padding);
+        megapixelsText.setAttribute(`y`, m.bottom);
+        megapixelsText.style.fontSize = `${size}px`;
+
+        svg.appendChild(megapixelsText);
+
+        megapixelsText.style.fontSize = `${size / (megapixelsText.getBBox().width / m.width)}px`;
+
+        const a = getRect(aspectText.getBBox());
+        const widthText = document.createElementNS(`http://www.w3.org/2000/svg`, `text`);
+
+        widthText.appendChild(document.createTextNode(width));
+        widthText.classList.add(`hp-class-width`);
+        widthText.setAttribute(`x`, width / 2);
+        widthText.setAttribute(`y`, c.horizontal);
+        widthText.style.fontSize = `${size / 2}px`;
+
+        svg.appendChild(widthText);
+
+        const d = size * 0.1;
+        const w = getRect(widthText.getBBox());
+        const rightArrow = document.createElementNS(`http://www.w3.org/2000/svg`, `path`);
+
+        rightArrow.setAttribute(`d`, `
+            M ${w.right + d}    ${w.horizontal - d}
+            L ${c.left - d * 4} ${w.horizontal - d}
+            L ${c.left - d * 4} ${w.horizontal - d * 2}
+            L ${c.left - d}     ${w.horizontal}
+            L ${c.left - d * 4} ${w.horizontal + d * 2}
+            L ${c.left - d * 4} ${w.horizontal + d}
+            L ${w.right + d}    ${w.horizontal + d}
+            Z`);
+
+        svg.appendChild(rightArrow);
+
+        const leftArrow = document.createElementNS(`http://www.w3.org/2000/svg`, `path`);
+
+        leftArrow.setAttribute(`d`, `
+            M ${w.left - d}      ${w.horizontal - d}
+            L ${a.right + d * 4} ${w.horizontal - d}
+            L ${a.right + d * 4} ${w.horizontal - d * 2}
+            L ${a.right + d}     ${w.horizontal}
+            L ${a.right + d * 4} ${w.horizontal + d * 2}
+            L ${a.right + d * 4} ${w.horizontal + d}
+            L ${w.left - d}      ${w.horizontal + d}
+            Z`);
+
+        svg.appendChild(leftArrow);
+
+        const heightText = document.createElementNS(`http://www.w3.org/2000/svg`, `text`);
+
+        heightText.appendChild(document.createTextNode(height));
+        heightText.classList.add(`hp-class-height`);
+        heightText.setAttribute(`x`, width);
+        heightText.setAttribute(`y`, height / 2);
+        heightText.style.fontSize = `${size / 2}px`;
+
+        svg.appendChild(heightText);
+
+        const h = getRect(heightText.getBBox());
+        const bottomArrow = document.createElementNS(`http://www.w3.org/2000/svg`, `path`);
+
+        bottomArrow.setAttribute(`d`, `
+            M ${h.vertical + d}     ${h.bottom + d}
+            L ${h.vertical + d}     ${c.top - d * 4}
+            L ${h.vertical + d * 2} ${c.top - d * 4}
+            L ${h.vertical}         ${c.top - d}
+            L ${h.vertical - d * 2} ${c.top - d * 4}
+            L ${h.vertical - d}     ${c.top - d * 4}
+            L ${h.vertical - d}     ${h.bottom + d}
+            Z`);
+
+        svg.appendChild(bottomArrow);
+
+        const mt = getRect(megapixelsText.getBBox());
+        const topArrow = document.createElementNS(`http://www.w3.org/2000/svg`, `path`);
+
+        topArrow.setAttribute(`d`, `
+            M ${h.vertical + d}     ${h.top - d}
+            L ${h.vertical + d}     ${mt.bottom + d * 4}
+            L ${h.vertical + d * 2} ${mt.bottom + d * 4}
+            L ${h.vertical}         ${mt.bottom + d}
+            L ${h.vertical - d * 2} ${mt.bottom + d * 4}
+            L ${h.vertical - d}     ${mt.bottom + d * 4}
+            L ${h.vertical - d}     ${h.top - d}
+            Z`);
+
+        svg.appendChild(topArrow);
+    }
+
+    svg.setAttribute(`viewBox`, `${viewBox.x} ${viewBox.y} ${viewBox.width} ${viewBox.height}`);
+}
