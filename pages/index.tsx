d1a0cc807dd7bcc9ce4290a6f4890910d4549eb0
hopeless programmer <me@hopeless-programmer.online>
2020-12-01 09:31:22 +0200

    Add next.js

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+export default class Index extends React.Component {
+    public render() {
+        return (
+            <h1 className={styles.h1}>
+                Безнадійний Програміст
+            </h1>
+        )
+    }
+}
