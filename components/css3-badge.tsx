47a4a1f8dbd7127e4130e8165eb3be57454639d3
hopeless programmer <me@hopeless-programmer.online>
2020-12-02 03:37:17 +0200

    Add icons

--- components/css3-badge.tsx
+++ components/css3-badge.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import styles from './css3-badge.module.scss'
+import { Css3 } from '@icons-pack/react-simple-icons'
 
 export default class CSS3Badge extends React.Component {
     public render() {
         return (
-            <abbr className={styles.abbr} title='Cascading Style Sheets'>
-                <a href='https://en.wikipedia.org/wiki/CSS#CSS_3'>CSS3</a>
-            </abbr>
+            <a href='https://en.wikipedia.org/wiki/CSS#CSS_3'>
+                <Css3 color='#1572B6'/>
+                <abbr className={styles.abbr} title='Cascading Style Sheets'>
+                    CSS3
+                </abbr>
+            </a>
         )
     }
 }
--- components/nextjs-badge.tsx
+++ components/nextjs-badge.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import styles from './nextjs-badge.module.scss'
+import { NextDotJs } from '@icons-pack/react-simple-icons'
 
 export default class NextJSBadge extends React.Component {
     public render() {
         return (
-            <a className={styles.a} href='https://en.wikipedia.org/wiki/Next.js'>Next.js</a>
+            <a className={styles.a} href='https://en.wikipedia.org/wiki/Next.js'>
+                <NextDotJs color='#000000'/>
+                Next.js
+            </a>
         )
     }
 }
