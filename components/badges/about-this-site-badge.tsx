11cd2678ee178de24472bd99b57cdf94e14329bd
hopeless programmer <me@hopeless-programmer.online>
2021-01-02 02:18:24 +0200

    Add about

--- components/badges/about-this-site-badge.tsx
+++ components/badges/about-this-site-badge.tsx
@@ -6,7 +6,7 @@ import styles from './about-this-site-badge.module.scss'
 export default class AboutThisSiteBadge extends Badge {
     protected Icon = Icon
     protected Caption = Caption
-    // protected href = 'https://en.wikipedia.org/wiki/CSS#CSS_3'
+    protected href = '/about'
 }
 
 class Icon extends React.Component {
