e8e1b65f72f17f5039069672ec4ffab4badeb8d1
hopeless programmer <me@hopeless-programmer.online>
2021-01-07 11:07:29 +0200

    Add vector reflection

---
+++ data/vector-reflection.tsx
@@ -0,0 +1,14 @@
+import { article, illustration, paragraph, section, sentence, tex, } from "../classes/shortcuts";
+
+const r = String.raw
+
+export default article(`Векторна проекція. `, ...[
+    section(``, ...[
+        paragraph(...[
+            sentence(``),
+        ]),
+        illustration(``, tex(r`
+            \vec{a'} = 2 proj(\vec{a},\vec{b}) - \vec{a} = 2\vec{b}\frac{(\vec{a},\vec{b})}{\vec{b}^2} - \vec{a}
+        `)),
+    ]),
+])
