df5bf12bb7fb3419c0a8e5878c575382d6f8b5a3
hopeless programmer <me@hopeless-programmer.online>
2021-03-09 05:29:29 +0200

    Update wip

--- pages/wip.tsx
+++ pages/wip.tsx
@@ -3,7 +3,6 @@ import ArticleLink from '../components/article-link'
 import Page from '../components/page'
 import projection from '../data/vector-projection'
 import reflection from '../data/vector-reflection'
-import intersection from '../data/lines-intersection'
 import pointCast from '../data/sphere-point-cast'
 import lineCast from '../data/sphere-line-cast'
 import l0 from '../data/l0'
@@ -20,7 +19,6 @@ export default class WIP extends React.Component {
             <Page title='В розробці'>
                 <ArticleLink model={projection} href='./articles/vector-projection'/>
                 <ArticleLink model={reflection} href='./articles/vector-reflection'/>
-                <ArticleLink model={intersection} href='./articles/lines-intersection'/>
                 <ArticleLink model={pointCast} href='./articles/sphere-point-cast'/>
                 <ArticleLink model={lineCast} href='./articles/sphere-line-cast'/>
                 <ArticleLink model={l0} href='./articles/l0'/>
