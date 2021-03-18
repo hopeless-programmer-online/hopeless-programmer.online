7d1827c4d47f58d91c2fc11b4b86c0299f7eeb81
hopeless programmer <me@hopeless-programmer.online>
2021-03-19 00:38:03 +0200

    Update trilateration.tsx

--- pages/articles/trilateration.tsx
+++ pages/articles/trilateration.tsx
@@ -6,7 +6,7 @@ import model from '../../data/trilateration'
 export default class Keywords extends React.Component {
     public render() {
         return (
-            <Page title=''>
+            <Page title='Знаходження точки за набором відстаней до неї. Трилатерація. '>
                 <Article model={model}/>
             </Page>
         )
