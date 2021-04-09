9f96857e22b7b333fb1c38f9fac8cb1d6786912d
hopeless programmer <me@hopeless-programmer.online>
2021-04-09 20:05:53 +0300

    Update sphere-plane-cast.tsx

--- data/sphere-plane-cast.tsx
+++ data/sphere-plane-cast.tsx
@@ -9,6 +9,33 @@ export default article(`Знаходження точки перетину ру�
             sentence(``),
         ]),
         illustration(``, tex(r`
+            \begin{cases}
+                \vec{x} &= \vec{a} + t \vec{b}
+                \\
+                (\vec{y} - \vec{p}) \vec{n} &= 0
+                \\
+                |\vec{y} - \vec{x}| &= r
+                \\
+                \dfrac{ \vec{x} - \vec{y} }{ |\vec{x} - \vec{y}| } \dfrac{ \vec{n} }{ |\vec{n}| } &= \pm 1
+            \end{cases}
+        `)),
+        illustration(``, tex(r`
+            \begin{cases}
+                \vec{x} &= \vec{a} + t \vec{b}
+                \\
+                \vec{y} \vec{n} &= \vec{p} \vec{n}
+                \\
+                (\vec{x} - \vec{y}) \vec{n} &= \pm r |\vec{n}|
+            \end{cases}
+        `)),
+        illustration(``, tex(r`
+            (\vec{a} + t \vec{b}) \vec{n} - \vec{p} \vec{n} = \pm r |\vec{n}|
+        `)),
+        illustration(``, tex(r`
+            t \vec{b} \vec{n} + \vec{a} \vec{n} - \vec{p} \vec{n} = \pm r |\vec{n}|
+        `)),
+        illustration(``, tex(r`
+            t = \dfrac{ (\vec{p} - \vec{a}) \vec{n} \pm r |\vec{n}| }{ \vec{b} \vec{n} }
         `)),
     ]),
 ])
