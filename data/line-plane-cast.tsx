24f0f44fe367f3d82ade1b288a6e53c948421d24
hopeless programmer <me@hopeless-programmer.online>
2021-04-09 20:05:50 +0300

    Update line-plane-cast.tsx

--- data/line-plane-cast.tsx
+++ data/line-plane-cast.tsx
@@ -12,17 +12,17 @@ export default article(`Знаходження точки перетину пр�
             \begin{cases}
                 \vec{x} &= \vec{a} + t \vec{b}
                 \\
-                (\vec{x} - \vec{o}) \cdot \vec{n} &= 0
+                (\vec{x} - \vec{o}) \vec{n} &= 0
             \end{cases}
         `)),
         illustration(``, tex(r`
-            (\vec{a} + t \vec{b} - \vec{o}) \cdot \vec{n} = 0
+            (\vec{a} + t \vec{b} - \vec{o}) \vec{n} = 0
         `)),
         illustration(``, tex(r`
-            t \vec{b} \cdot \vec{n} + (\vec{a} - \vec{o}) \cdot \vec{n} = 0
+            t \vec{b} \vec{n} + (\vec{a} - \vec{o}) \vec{n} = 0
         `)),
         illustration(``, tex(r`
-            t = \dfrac{ (\vec{o} - \vec{a}) \cdot \vec{n} }{ \vec{b} \cdot \vec{n} }
+            t = \dfrac{ (\vec{o} - \vec{a}) \vec{n} }{ \vec{b} \vec{n} }
         `)),
     ]),
 ])
