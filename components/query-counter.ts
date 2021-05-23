04743356c46d18e4ee81d9ccc482e733c10937e0
hopeless programmer <me@hopeless-programmer.online>
2021-05-23 19:23:31 +0300

    Fix database crash

--- components/query-counter.ts
+++ components/query-counter.ts
@@ -2,10 +2,9 @@ import { Http2ServerRequest } from "http2";
 import connect from "./database";
 
 export default async function queryCounter({ url } : Http2ServerRequest) {
-    const { pathname } = new URL(`http://x.com${url}`)
-    const client = await connect()
-
     try {
+        const { pathname } = new URL(`http://x.com${url}`)
+        const client = await connect()
         const { rows } = await client.query(`SELECT value FROM counters WHERE id = $1;`, [ pathname ])
 
         // console.log(rows)
@@ -29,5 +28,4 @@ export default async function queryCounter({ url } : Http2ServerRequest) {
 
         return 0
     }
-
 }
