688b0abeccfdf9437d9ca8265b1e90d08f8657c3
hopeless programmer <me@hopeless-programmer.online>
2022-02-03 03:51:46 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import segregation from '../data/segregation'
 import symmetry from '../data/symmetry'
 import visitor from '../data/visitor'
 import linesIntersection from '../data/lines-intersection'
+import comments from '../data/comments'
 import { Http2ServerRequest } from 'http2'
 import queryCounter from '../components/query-counter'
 
@@ -20,14 +21,15 @@ export default class Index extends React.Component<Props> {
     public render() {
         return (
             <Page counter={this.props.counter}>
-                <ArticleLink model={overloading} href='./articles/result-overloading'/>
-                <ArticleLink model={overlapping} href='./articles/irreversible-overlapping'/>
-                <ArticleLink model={keywords} href='./articles/keywords'/>
-                <ArticleLink model={segregation} href='./articles/segregation'/>
-                <ArticleLink model={symmetry} href='./articles/symmetry'/>
-                <ArticleLink model={visitor} href='./articles/visitor'/>
-                <ArticleLink model={virtualOverlapping} href='./articles/virtual-overlapping'/>
+                <ArticleLink model={comments} href='./articles/comments'/>
                 <ArticleLink model={linesIntersection} href='./articles/lines-intersection'/>
+                <ArticleLink model={virtualOverlapping} href='./articles/virtual-overlapping'/>
+                <ArticleLink model={segregation} href='./articles/segregation'/>
+                <ArticleLink model={visitor} href='./articles/visitor'/>
+                <ArticleLink model={symmetry} href='./articles/symmetry'/>
+                <ArticleLink model={keywords} href='./articles/keywords'/>
+                <ArticleLink model={overlapping} href='./articles/irreversible-overlapping'/>
+                <ArticleLink model={overloading} href='./articles/result-overloading'/>
             </Page>
         )
     }
